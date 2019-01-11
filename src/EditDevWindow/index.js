b304ea9902603e1e39b9442fd50fc7aaf636149e
Karasev DL work <karasev.dmitry@gmail.com>
2019-01-11 16:37:18 +0400

    fetch data

--- src/EditDevWindow/index.js
+++ src/EditDevWindow/index.js
@@ -15,6 +15,9 @@ import Modules from '../components/Modules'
 // import RemoteDataProvider from "../components/Base/RemoteDataProvider"
 
 const DEV_DATA_URL = 'http://netcmdb-loc.rs.ru:8082/api/getDevData.json'
+const DEV_MODULES_DATA_URL = 'http://netcmdb-loc.rs.ru:8082/api/getDevModulesData.json'
+const DEV_PORTS_DATA_URL = 'http://netcmdb-loc.rs.ru:8082/api/getDevPortsData.json'
+
 
 class EditDevWindow extends Component {
     constructor(props, context) {
@@ -23,27 +26,53 @@ class EditDevWindow extends Component {
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
 
-        this.state = {
-            show: true,
-            devId: 11800,
-            devDataLoading: false,
-            devDataReady: false,
 
-            region_id: null,
-            city_id: null,
-            office_id: null,
-            devType_id: null,
-            software_id: null,
-            softwareVer: null,
-            sn: null,
-            altSn: null,
-            hostname: null,
-            mngIp: null,
-            officeComment: null,
-            deviceComment: null,
-            devInUse: false
-        };
     }
+    /**
+     *
+     * @type {object} state
+     * @property {boolean} show
+     * @property {number} devId
+     * @property {boolean} devDataLoading
+     * @property {boolean} devDataReady
+     * @property {object} locationInfo
+     * @property {object} devInfo
+     * @property {object} modulesInfo
+     * @property {object} portsInfo
+     *
+     */
+    state = {
+        show: true,
+        devId: 11800,
+        // devDataLoading: false,
+        devDataReady: false,
+        /**
+         * @type {{region_id, city_id, office_id}}
+         */
+        locationInfo: {
+            region_id: '',
+            city_id: '',
+            office_id: ''
+        },
+        /**
+         * @type {{dev_id, location_id, platform_id, platform_item_id,
+         * 'software_id', 'software_item_id', 'vendor_id', 'dev_type_id',
+         * 'dev_comment', 'software_comment', 'dev_last_update', 'dev_in_use',
+         * 'platform_sn', 'platform_sn_alt', 'is_hw', 'software_ver', 'dev_details',
+         * 'software_details'}} devInfo
+         */
+        devInfo: {},
+        /**
+         * @type {{'module' ,'module_item_id', 'module_item_details',
+         * 'module_item_sn', 'module_in_use', 'module_not_found'}}
+         */
+        modulesInfo: {},
+        /**
+         * @type {{'port_id', 'port_ip', 'port_comment',
+         * 'port_details', 'port_is_mng', 'port_mac', 'port_mask_len'}}
+         */
+        portsInfo: {}
+    };
     cityFilter = {
         accessor: 'region_id',
         statement: '=',
@@ -53,6 +82,36 @@ class EditDevWindow extends Component {
         accessor: 'city_id',
         statement: '=',
         value: ''
+    }
+
+    initialData = {
+        /**
+         * @type {{dev_id, location_id, platform_id, platform_item_id,
+         * 'software_id', 'software_item_id', 'vendor_id', 'dev_type_id',
+         * 'dev_comment', 'software_comment', 'dev_last_update', 'dev_in_use',
+         * 'platform_sn', 'platform_sn_alt', 'is_hw', 'software_ver', 'dev_details',
+         * 'software_details'}} devInfo
+         */
+        devInfo: {},
+        /**
+         * @type {{'module' ,'module_item_id', 'module_item_details',
+         * 'module_item_sn', 'module_in_use', 'module_not_found'}}
+         */
+        modulesInfo: {},
+        /**
+         * @type {{'port_id', 'port_ip', 'port_comment',
+         * 'port_details', 'port_is_mng', 'port_mac', 'port_mask_len'}}
+         */
+        portsInfo: [],
+        /**
+         * @type {string} mngIp
+         */
+        mngIp: ''
+    }
+    managementIp = (portsInfo) => {
+        if (!check.array(portsInfo)) return
+        const res = portsInfo.filter((port) => port.port_is_mng).map((port) => port.port_ip)
+        return res.join(', ')
     }
 
     handleClose() {
@@ -68,15 +127,18 @@ class EditDevWindow extends Component {
 
     onChangeRegion = ({selected}) => {
         console.log('onChangeRegion: ', selected)
-        this.setState({region_id: selected})
+        const {locationInfo} = this.state
+        this.setState({locationInfo: Object.assign({}, locationInfo, {region_id: selected})})
     }
     onChangeCity = ({selected}) => {
         console.log('onChangeCity: ', selected)
-        this.setState({city_id: selected})
+        const {locationInfo} = this.state
+        this.setState({locationInfo: Object.assign({}, locationInfo, {city_id: selected})})
     }
     onChangeOffice = ({selected}) => {
         console.log('onChangeOffice: ', selected)
-        this.setState({office_id: selected})
+        const {locationInfo} = this.state
+        this.setState({locationInfo: Object.assign({}, locationInfo, {office_id: selected})})
     }
     onChangeDevType = ({selected}) => {
         console.log('onChangeDevType: ', selected)
@@ -112,17 +174,53 @@ class EditDevWindow extends Component {
         this.setState({devInUse: e.target.checked})
     }
 
-    loadDeviceData = async (id) => {
+    fetchDeviceData = async (id) => {
         try {
             const res = await axios.get(DEV_DATA_URL, {
                 params: {id}
             })
-            const {data: {devData}} = res
-            // console.log('devData:', devData)
-            return check.nonEmptyObject(devData) ? devData : {}
+            const {data} = res
+            if (!data.devInfo) {
+                console.log('ERROR: fetchDeviceData')
+                return {}
+            }
+            return data
         } catch (e) {
-            console.log('fetching data error:', e.toString())
+            console.log('ERROR: fetchDeviceData', e.toString())
             return {}
+        }
+    }
+    fetchDevModulesData = async (id) => {
+        try {
+            const res = await axios.get(DEV_MODULES_DATA_URL, {
+                params: {id}
+            })
+            const {data} = res
+            // console.log('modData:', modData)
+            if (!data.modulesInfo) {
+                console.log('ERROR: fetchDevModulesData')
+                return []
+            }
+            return data
+        } catch (e) {
+            console.log('ERROR: fetchDevModulesData', e.toString())
+            return []
+        }
+    }
+    fetchDevPortsData = async (id) => {
+        try {
+            const res = await axios.get(DEV_PORTS_DATA_URL, {
+                params: {id}
+            })
+            const {data} = res
+            if (!data.portsInfo) {
+                console.log('ERROR: fetchDevPortsData')
+                return []
+            }
+            return data
+        } catch (e) {
+            console.log('ERROR: fetchDevPortsData', e.toString())
+            return []
         }
     }
 
@@ -139,7 +237,7 @@ class EditDevWindow extends Component {
     })('')
 
     render() {
-
+        const {devInfo, modulesInfo, portsInfo, mngIp} = this.initialData
         return (
             <Modal show={this.state.show} onHide={this.handleClose} bsSize="large" >
                 <ModalHeader closeButton>
@@ -147,8 +245,8 @@ class EditDevWindow extends Component {
                 </ModalHeader>
                 <ModalBody className={custCss.modalBody} >
                     <Row>
-                        <Col md={4}><Region onChange={this.onChangeRegion} selected={this.state.region_id}/></Col>
-                        <Col md={4}><City onChange={this.onChangeCity} selected={this.state.city_id} filter={this.cityFilter}/></Col>
+                        <Col md={4}><Region onChange={this.onChangeRegion} selected={this.state.locationInfo.region_id}/></Col>
+                        <Col md={4}><City onChange={this.onChangeCity} selected={this.state.locationInfo.city_id} filter={this.cityFilter}/></Col>
                         <Col md={4}><Office onChange={this.onChangeOffice}/></Col>
                     </Row>
                     <Row>
@@ -158,10 +256,10 @@ class EditDevWindow extends Component {
                         <Col md={3}><Input controlId='swVer' onChange={this.onChangeSoftwareVer} defaultValue='по версия' label="Версия ПО"/></Col>
                     </Row>
                     <Row>
-                        <Col md={3}><Input controlId='devSn' addOnPosition="left" addOnText="SN" onChange={this.onChangeSn} defaultValue='test' label=" " disabled/></Col>
-                        <Col md={3}><Input controlId='devAltSn' addOnPosition="left" addOnText="alt SN" onChange={this.onChangeAltSn} label=" " /></Col>
-                        <Col md={3}><Input controlId='hostname' addOnPosition="left" addOnText="hostname" onChange={this.onChangeHostname} label=" " /></Col>
-                        <Col md={3}><Input controlId='managementIP' addOnPosition="left" addOnText="management IP" onChange={this.onChangeMngIp} label=" " defaultValue='255.255.255.255' /></Col>
+                        <Col md={3}><Input controlId='devSn' addOnPosition="left" addOnText="SN" onChange={this.onChangeSn} defaultValue={devInfo.platform_sn} label=" " disabled/></Col>
+                        <Col md={3}><Input controlId='devAltSn' addOnPosition="left" addOnText="alt SN" onChange={this.onChangeAltSn} defaultValue={devInfo.platform_sn_alt} label=" " /></Col>
+                        <Col md={3}><Input controlId='hostname' addOnPosition="left" addOnText="hostname" onChange={this.onChangeHostname} defaultValue={devInfo.dev_details && devInfo.dev_details.hostname} label=" " /></Col>
+                        <Col md={3}><Input controlId='managementIP' addOnPosition="left" addOnText="management IP" onChange={this.onChangeMngIp} label=" " defaultValue={mngIp} /></Col>
                     </Row>
                     <Row>
                         <Col md={6}><TextArea controlId="officeComment" onChange={this.onChangeOfficeComment} defaultValue='officeComment' label="Комментарий к оффису" /></Col>
@@ -193,25 +291,19 @@ class EditDevWindow extends Component {
         const {devId, devDataReady, devDataLoading} = this.state
         if (devId && !devDataReady && !devDataLoading) {
             console.log('loading dev data')
+            const {devId} = this.state
             this.setState({devDataLoading: true})
-            const devData = await this.loadDeviceData(this.state.devId)
-            console.log(devData)
-            const data = (({
-                               region_id = '',
-                               city_id = '',
-                               office_id = '',
-                               devType_id = '',
-                               software_id = '',
-                               softwareVer = '',
-                               sn = '',
-                               altSn = '',
-                               hostname = '',
-                               mngIp = '',
-                               officeComment = '',
-                               deviceComment = '',
-                               devInUse = false
-            }) => ({region_id, city_id, office_id, devType_id, software_id, softwareVer, sn, altSn, hostname, mngIp, officeComment, deviceComment, devInUse}))(devData)
-            this.setState(Object.assign({}, {devDataLoading: false, devDataReady: true}, data))
+            // const devData = await this.fetchDeviceData(this.state.devId)
+            const response = await Promise.all([
+                this.fetchDeviceData(devId),
+                this.fetchDevModulesData(devId),
+                this.fetchDevPortsData(devId)
+            ])
+            const [{devInfo}, {modulesInfo}, {portsInfo}] = response
+            console.log('devData:',devInfo, modulesInfo, portsInfo)
+            this.initialData = {devInfo, modulesInfo, portsInfo, mngIp: this.managementIp(portsInfo)}
+            console.log('22222', this.initialData)
+            this.setState(Object.assign({}, {devDataLoading: false, devDataReady: true}, {devInfo, modulesInfo, portsInfo}))
         }
     }
 }
